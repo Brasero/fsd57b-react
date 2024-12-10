240518147201fe8d11ef057fde55b845b5722e3e
Brasero <brandonricci4@gmail.com>
2024-12-10 15:21:16 +0100

    ajout des commentaires

--- shufflePair/src/component/ListGenerator.jsx
+++ shufflePair/src/component/ListGenerator.jsx
@@ -4,9 +4,11 @@ import UnordonateList from "./UnordonateList.jsx";
 import OrdonateList from "./OrdonateList.jsx";
 
 const ListGenerator = ( ) => {
- 
+ // On prévoit un state pour stocker la list aléatoire générée par la fonction "generate"
  const [unordonateList, setUnordonateList] = useState([])
  
+ // Lors du click sur le bouton "Générer une liste" ont appel la fonction "generate" et on enregistre le résultat dans
+ // le state, on passera ensuite ce tableau au component "UnordonateList" et "OrdonateList" qui seront chargée de l'afficher
  const handleGenerate = () => {
   const list = generate(10)
   setUnordonateList(list)
